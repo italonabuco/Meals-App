888f917eb7535fc55c03a53d7150f400cdcd33d3
Italo Nabuco <italonabuco@hotmail.com>
2020-08-08 18:22:39 -0300

    Creating HeaderButton component - installing react-navigation-header-buttons

---
+++ components/HeaderButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Platform} from 'react-native';
+import {HeaderButton} from 'react-navigation-header-buttons';
+import Icon from 'react-native-vector-icons/AntDesign';
+
+import Colors from '../constants/Colors';
+
+const CustomHeaderButton = (props) => {
+  return (
+    <HeaderButton
+      {...props}
+      IconComponent={Icon}
+      iconSize={23}
+      color={Platform.OS === 'android' ? 'white' : Colors.primaryColor}
+    />
+  );
+};
+
+export default CustomHeaderButton;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,11 @@
 import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
 import {MEALS} from '../data/dummy-data';
 
 const MealDetailScreen = (props) => {
-  const {text, route, navigation} = props;
+  const {route, navigation} = props;
   const {mealId} = route.params || {};
 
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
@@ -11,6 +13,15 @@ const MealDetailScreen = (props) => {
   useEffect(() => {
     navigation.setOptions({
       title: selectedMeal.title,
+      headerRight: () => (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Favortie"
+            iconName="star"
+            onPress={() => console.log(selectedMeal.title)}
+          />
+        </HeaderButtons>
+      ),
     });
   }, [selectedMeal]);
 
