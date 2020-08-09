dff27023f177939f2dcfb9b9fb5018505ee484be
Italo Nabuco <italonabuco@hotmail.com>
2020-08-09 10:03:57 -0300

    Adding Icons to createBottomTabNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
-
 import FavoriteScreen from '../screens/FavoritesScreen';
 
 import Colors from '../constants/Colors.js';
@@ -51,9 +51,27 @@ const MealsNavigator = (props) => (
 const Tab = createBottomTabNavigator();
 
 const MealsFavTabNavigator = (props) => (
-  <Tab.Navigator>
-    <Tab.Screen name="Meals" component={MealsNavigator}/>
-    <Tab.Screen name="Favorites" component={FavoriteScreen}/>
+  <Tab.Navigator
+    tabBarOptions={{activeTintColor: Colors.accentColor}}
+    screenOptions={({route}) => ({
+      tabBarIcon: ({focused, color, size}) => {
+        let iconName;
+        switch (route.name) {
+          case 'Meals':
+            iconName = focused ? 'restaurant' : 'restaurant-outline';
+            break;
+          case 'Favorites':
+            iconName = focused ? 'star' : 'star-outline';
+            break;
+          default:
+            iconName = 'home';
+            break;
+        }
+        return <Icon name={iconName} color={color} size={size} />;
+      },
+    })}>
+    <Tab.Screen name="Meals" component={MealsNavigator} options={{tabBarLabel: 'Meals'}}/>
+    <Tab.Screen name="Favorites" component={FavoriteScreen} options={{tabBarLabel: 'Favorites!'}}/>
   </Tab.Navigator>
 );
 
